b1102c47c8a73e8113ef44cece5932c49b58ed8e
jinju420 <ljjshlove@nate.com>
2023-03-15 14:53:43 +0900

    Image컴포넌트로 origin 랜던이미지 출력

---
+++ components/Banner.tsx
@@ -0,0 +1,31 @@
+import { Movie } from '@/typings';
+import { useEffect, useState } from 'react';
+import Image from 'next/image';
+import { baseURL } from '@/constants/movie';
+
+interface Props {
+	original: Movie[];
+}
+
+function Banner({ original }: Props) {
+	const [Movie, setMovie] = useState<Movie | null>(null);
+
+	useEffect(() => {
+		//0~20사이의 랜덤 정수값을 반환하는 공식
+		//Math.floor(Math.random() * 20)
+		console.log(Math.floor(Math.random() * original.length - 1));
+		setMovie(original[Math.floor(Math.random() * original.length - 1)]);
+	}, [original]);
+
+	console.log(Movie);
+
+	return (
+		<section>
+			<div>
+				<Image src={`${baseURL}${Movie?.backdrop_path}`} alt={`Movie?.title`} fill className='object-cover' />
+			</div>
+		</section>
+	);
+}
+
+export default Banner;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,24 @@
 import Header from '@/components/Header';
 import type { NextPage } from 'next';
 import Head from 'next/head';
+import requests from '@/utils/requests';
+import { Movie } from '@/typings';
+import Banner from '@/components/Banner';
 
-const Home: NextPage = () => {
+interface IndexProps {
+	original: Movie[];
+	topRated: Movie[];
+	crime: Movie[];
+	drama: Movie[];
+	fantasy: Movie[];
+	action: Movie[];
+	animation: Movie[];
+}
+
+const Home: NextPage<IndexProps> = ({ original, topRated, crime, drama, fantasy, action, animation }) => {
+	// console.log(original);
 	return (
-		<div>
+		<div className='relative h-screen bg-gradient-to-b from-[#333] to-[#141414]'>
 			<Head>
 				<title>Netflix Clone-coding</title>
 				<link rel='icon' href='/favicon.ico' />
@@ -12,6 +26,7 @@ const Home: NextPage = () => {
 
 			<Header />
 			<main className=''>
+				<Banner original={original} />
 				<section></section>
 			</main>
 		</div>
@@ -19,3 +34,27 @@ const Home: NextPage = () => {
 };
 
 export default Home;
+
+export const getServerSideProps = async () => {
+	const [original, top, crime, drama, fantasy, action, animation] = await Promise.all([
+		fetch(requests.original).then((res) => res.json()),
+		fetch(requests.top).then((res) => res.json()),
+		fetch(requests.crime).then((res) => res.json()),
+		fetch(requests.drama).then((res) => res.json()),
+		fetch(requests.fantasy).then((res) => res.json()),
+		fetch(requests.action).then((res) => res.json()),
+		fetch(requests.animation).then((res) => res.json()),
+	]);
+
+	return {
+		props: {
+			original: original.results,
+			topRated: top.results,
+			crime: crime.results,
+			drama: drama.results,
+			fantasy: fantasy.results,
+			action: action.results,
+			animation: animation.results,
+		},
+	};
+};
