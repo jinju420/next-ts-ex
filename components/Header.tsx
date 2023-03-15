94401a352a1cb37c0cd36f9a4f9467ba8da19564
jinju420 <ljjshlove@nate.com>
2023-03-15 09:39:46 +0900

    init

---
+++ components/Header.tsx
@@ -0,0 +1,16 @@
+function Header() {
+	return (
+		<header>
+			<h1>GRANHAND.</h1>
+
+			<ul>
+				<li>menu</li>
+				<li>menu</li>
+				<li>menu</li>
+				<li>menu</li>
+			</ul>
+		</header>
+	);
+}
+
+export default Header;
---
+++ pages/index.tsx
@@ -0,0 +1,19 @@
+import Header from '@/components/Header';
+import type { NextPage } from 'next';
+import Head from 'next/head';
+
+const Home: NextPage = () => {
+	return (
+		<div className='flex min-h-screen flex-col items-center justify-center py-2'>
+			<Head>
+				<title>GRANHAND.</title>
+				<link rel='icon' href='/favicon.ico' />
+			</Head>
+
+			<Header />
+			<main className=''></main>
+		</div>
+	);
+};
+
+export default Home;
