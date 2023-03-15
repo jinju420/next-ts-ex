e87aae4efe36a6e4aefb94a47a390e8b0ff1275a
jinju420 <ljjshlove@nate.com>
2023-03-15 15:17:23 +0900

    TV데이터 인터페이스 추가

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -1,28 +1,28 @@
-import { Movie } from '@/typings';
+import { TV } from '@/typings';
 import { useEffect, useState } from 'react';
 import Image from 'next/image';
 import { baseURL } from '@/constants/movie';
 
 interface Props {
-	original: Movie[];
+	original: TV[];
 }
 
 function Banner({ original }: Props) {
-	const [Movie, setMovie] = useState<Movie | null>(null);
+	const [TV, setTV] = useState<TV | null>(null);
 
 	useEffect(() => {
 		//0~20사이의 랜덤 정수값을 반환하는 공식
 		//Math.floor(Math.random() * 20)
 		console.log(Math.floor(Math.random() * original.length - 1));
-		setMovie(original[Math.floor(Math.random() * original.length - 1)]);
+		setTV(original[Math.floor(Math.random() * original.length - 1)]);
 	}, [original]);
 
-	console.log(Movie);
+	console.log(TV);
 
 	return (
 		<section>
 			<div>
-				<Image src={`${baseURL}${Movie?.backdrop_path}`} alt={`Movie?.title`} fill className='object-cover' />
+				<Image src={`${baseURL}${TV?.backdrop_path}`} alt={`TV?.name`} priority fill quality={50} className='object-cover' />
 			</div>
 		</section>
 	);
